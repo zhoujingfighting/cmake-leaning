1a8986bc7aee25c42e5a55d01babb11ced490886
zhoujingfighting <254644528@qq.com>
2023-01-14 13:22:19 +0800

    Update option use

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
-#include <iostream>
-#include "sharedspice.h"
-#include "test/test.h"
-using namespace std;
-int add(int a, int b) { 
-  return a + b;
-  }
 
-  int main(){
-    
-    cout << add(4, 7) << endl;
-    cout << add(4, 7) << endl;
-    cout << _sub(1, 4) << endl;
-    return 0;
-  }
+#include "sharedspice.h"
+#include <iostream>
+#include "main.h"
+#if defined(USE_CUMSTOMIZED_LIB)
+#include "test/test.h"
+#endif
+using namespace std;
+int add(int a, int b) { return a + b; }
+
+int main() {
+
+  cout << add(4, 7) << endl;
+  #if defined(USE_CUMSTOMIZED_LIB)
+  cout << _sub(1,3) << endl;
+  #endif
+  return 0;
+}
